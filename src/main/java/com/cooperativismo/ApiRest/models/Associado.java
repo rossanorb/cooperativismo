fdeb7c636e502a6cc0e04b66bdd1d32ac69b2172
Rossano <rossanorb@gmail.com>
2019-11-09 14:46:00 -0300

    criado model associados

--- src/main/java/com/cooperativismo/ApiRest/models/Associado.java
+++ src/main/java/com/cooperativismo/ApiRest/models/Associado.java
@@ -5,16 +5,52 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
 public class Associado {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	private String nome;
-	
-	private String cpf;	
+
+	private String cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Associado() {
+	}
+
+	public Associado(String nome, String cpf) {
+		this.nome = nome;
+		this.cpf = cpf;
+	}
+
+	@Override
+	public String toString() {
+		return "{id: " + this.id + ",name: " + this.nome + ", cpf: " + this.cpf + "}";
+	}
 
 }
